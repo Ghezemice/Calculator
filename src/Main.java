2f943122f811a330812b884c07ddfeb32aea8fad
Penya <istvan.penyasko@zooshgroup.com>
2023-09-23 12:54:47 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("Give the operation and the numbers separated by space please.\n Possible operations: ADD, SUB, DIV, MUL.\n Example: ADD 1 2");
         Scanner scanner = new Scanner(System.in);
         String line = scanner.nextLine();
+        scanner.close();
 
         Fields field = parseLine(line);
 
