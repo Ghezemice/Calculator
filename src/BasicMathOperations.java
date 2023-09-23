7ffea224a59c1dbcbadc63888e1040ce5d53cb33
Penya <istvan.penyasko@zooshgroup.com>
2023-09-23 14:56:07 +0200

    New unit tests for calculator functions

--- src/BasicMathOperations.java
+++ src/BasicMathOperations.java
@@ -8,10 +8,14 @@ public class BasicMathOperations {
     }
 
     public static float div(int a, int b) {
-        return a / b;
+        try {
+            return a / b;
+        } catch (ArithmeticException e) {
+            return Float.NaN;
+        }
     }
 
     public static int mul(int a, int b) {
-        return a * b * -1;
+        return a * b;
     }
 }
--- src/BasicMathOperationsTest.java
+++ src/BasicMathOperationsTest.java
@@ -7,11 +7,35 @@ public class BasicMathOperationsTest {
     public void testAdd() {
         Assert.assertEquals(1, BasicMathOperations.add(0, 1));
         Assert.assertEquals(0, BasicMathOperations.add(-1, 1));
+        Assert.assertEquals(-2, BasicMathOperations.add(-1, -1));
     }
 
     @Test
-    public void testStub() {
+    public void testSub() {
         Assert.assertEquals(-1, BasicMathOperations.sub(0, 1));
         Assert.assertEquals(0, BasicMathOperations.sub(-1, -1));
+        Assert.assertEquals(-2, BasicMathOperations.sub(-1, 1));
+        Assert.assertEquals(0, BasicMathOperations.sub(1, -1));
+    }
+
+    @Test
+    public void testDiv() {
+        Assert.assertEquals(0, BasicMathOperations.div(0, 1), 0);
+        Assert.assertEquals(1, BasicMathOperations.div(1, 1), 0);
+        Assert.assertEquals(2, BasicMathOperations.div(4, 2), 0);
+        Assert.assertEquals(1, BasicMathOperations.div(-1, -1), 0);
+        Assert.assertEquals(-2, BasicMathOperations.div(4, -2), 0);
+        Assert.assertEquals(-2, BasicMathOperations.div(-4, 2), 0);
+        Assert.assertEquals(Float.NaN, BasicMathOperations.div(1, 0), 0);
+    }
+
+    @Test
+    public void testMul() {
+        Assert.assertEquals(0, BasicMathOperations.mul(0, 1), 0);
+        Assert.assertEquals(1, BasicMathOperations.mul(1, 1), 0);
+        Assert.assertEquals(8, BasicMathOperations.mul(4, 2), 0);
+        Assert.assertEquals(1, BasicMathOperations.mul(-1, -1), 0);
+        Assert.assertEquals(-8, BasicMathOperations.mul(4, -2), 0);
+        Assert.assertEquals(-8, BasicMathOperations.mul(-4, 2), 0);
     }
 }
