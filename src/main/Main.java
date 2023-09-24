8434a9a44ba86eedc6953cfe85df7fb20fd1b895
Penya <istvan.penyasko@zooshgroup.com>
2023-09-24 14:23:46 +0200

    Adding example to SUM operation

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Give the operation and the numbers separated by space please.\n Possible operations: ADD, SUB, DIV, MUL.\n Example: ADD 1 2\n");
+        System.out.println("Give the operation and the numbers separated by space please.\n Possible operations: ADD, SUB, DIV, MUL, SUM.\n Example: ADD 1 2\n Using SUM you can add up multiple numbers.\n Example: SUM 1 2 3 4\n");
         Scanner scanner = new Scanner(System.in);
         String line = scanner.nextLine();
         scanner.close();
