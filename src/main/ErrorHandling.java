3423ca8b105ac8f6f90088af2cb2b0b2adea9c55
Penya <istvan.penyasko@zooshgroup.com>
2023-09-24 14:22:58 +0200

    Refactoring ErrorHandling

--- src/main/ErrorHandling.java
+++ src/main/ErrorHandling.java
@@ -30,21 +30,62 @@ public class ErrorHandling {
 
     public static String getErrorMessage(String[] strings) {
         String errorMessage = null;
-        if (strings.length < 3) {
+        if (isTooLessParameters(strings)) {
             errorMessage =  notEnoughParametersError + strings.length;
         }
-        else if (3 < strings.length && !strings[0].equals(Fields.Operation.SUM.toString())) {
+        else if (isTooManyParameters(strings)) {
             errorMessage =  tooManyParametersError + strings.length;
         }
-        else if (!checkOperationValidity(strings[0].toUpperCase())) {
+        else if (isOperationInvalid(strings)) {
             errorMessage = strings[0] + wrongOperation;
         }
-        else if (!isFloat(strings[1]) || !isFloat(strings[2])) {
+        else if (isParametersNumbers(strings)) {
             errorMessage = wrongNumberError;
         }
-        else if (strings[0].toUpperCase().equals(Fields.Operation.DIV.toString()) && strings[2].equals("0")) {
+        else if (isDivisionByZero(strings)) {
             errorMessage = divisionByZeroError;
         }
         return errorMessage;
+    }
+
+    private static boolean isTooLessParameters(String[] strings) {
+        if (strings.length < 3) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isTooManyParameters(String[] strings) {
+        if (3 < strings.length && !(strings[0].toUpperCase()).equals(Fields.Operation.SUM.toString())) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isOperationInvalid(String[] strings) {
+        if (!checkOperationValidity(strings[0].toUpperCase())) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isParametersNumbers(String[] strings) {
+        boolean allTheNumbersAreFloat = true;
+        for (int i = 1; i < strings.length; i++) {
+            if (!isFloat(strings[i])) {
+                allTheNumbersAreFloat = false;
+            }
+        }
+        if (!allTheNumbersAreFloat) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isDivisionByZero(String[] strings) {
+        if (strings[0].toUpperCase().equals(Fields.Operation.DIV.toString()) && strings[2].equals("0")) {
+            return true;
+        }
+        return false;
     }
 }
